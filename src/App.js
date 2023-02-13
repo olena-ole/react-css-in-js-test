a565200728ac7a8be7cab0e63422bbd4126ceabc
Olena <pampam32121@gmail.com>
2023-02-13 20:21:26 +0200

    practiced props.render() pattern

--- src/App.js
+++ src/App.js
@@ -106,9 +106,42 @@ const HelloGreeting = () => {
   )
 }
 
+const Message = (props) => {
+  return (
+    <h2>The counter is {props.counter}</h2>
+  )
+}
+
+class Counter extends Component {
+  state = {
+    counter: 0
+  }
+
+  changeCounter = () => {
+    this.setState(({counter}) => ({counter: counter + 1}))
+  }
+
+  render() {
+    return (
+      <>
+        <button
+          className='btn btn-primary'
+          onClick={this.changeCounter}>
+          Click me!
+        </button>
+        {/* <Message counter={this.state.counter}/> won't use this for reusability reasons*/} 
+        {this.props.render(this.state.counter)}
+      </>
+    )
+  }
+}
+
 function App() {
   return (
     <Wrapper>
+      <Counter render={counter => {
+        return <Message counter={counter}/>
+      }}/>
       <HelloGreeting />
       <BootstrapTest 
         left={
